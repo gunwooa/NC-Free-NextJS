99afb025e618fb2729819bbe0f5213a88bdebf1f
gw.jang <gw.jang@cardoc.kr>
2022-04-07 00:08:16 +0900

    1.2 Static Pre Rendering

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 시작
+
+[강의 사이트](https://nomadcoders.co/nextjs-fundamentals/lectures/3454)
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
+
 export default function Home() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div>
-      <h1>안녕~</h1>
+      <h1>안녕~ {counter}</h1>
+      <button onClick={() => setCounter((prev) => prev + 1)}>+</button>
     </div>
   );
 }
