b11925ea18a134c17fbd704190e062f30dd39d45
gw.jang <gw.jang@cardoc.kr>
2022-04-07 01:38:11 +0900

    1.4 CSS Modules

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,20 +1,30 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "./NavBar.module.css";
 
-const NavBar = () => {
+export default function NavBar() {
   const router = useRouter();
   return (
     <nav>
       <Link href="/">
-        <a style={{ color: router.pathname === "/" ? "red" : "black" }}>Home</a>
+        <a
+          className={`${styles.link} ${
+            router.pathname === "/" ? styles.active : ""
+          }`}
+        >
+          Home
+        </a>
       </Link>
       <Link href="/about">
-        <a style={{ color: router.pathname === "/about" ? "red" : "black" }}>
+        <a
+          className={[
+            styles.link,
+            router.pathname === "/about" ? styles.active : "",
+          ].join(" ")}
+        >
           About
         </a>
       </Link>
     </nav>
   );
-};
-
-export default NavBar;
+}
