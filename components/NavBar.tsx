2f4a54b2a47a21864de34621b2146bf4d5eb5e4b
gw.jang <gw.jang@cardoc.kr>
2022-04-09 18:12:45 +0900

    2.1 Fetching Data

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -6,16 +6,45 @@ export default function NavBar() {
 
   return (
     <nav>
-      <Link href="/">
-        <a className={router.pathname === "/" ? "active" : ""}>Home</a>
-      </Link>
-      <Link href="/about">
-        <a className={router.pathname === "/about" ? "active" : ""}>About</a>
-      </Link>
+      <img src="/vercel.svg" />
+      <div>
+        <Link href="/">
+          <a className={router.pathname === "/" ? "active" : ""}>Home</a>
+        </Link>
+        <Link href="/about">
+          <a className={router.pathname === "/about" ? "active" : ""}>About</a>
+        </Link>
+      </div>
 
       <style jsx>{`
+        nav {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          gap: 10px;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        }
+
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
+        }
+
         .active {
           color: tomato;
+        }
+
+        nav div {
+          display: flex;
+          gap: 10px;
         }
       `}</style>
     </nav>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,49 @@
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
+const API_KEY = "d3d3d680cf43c8a9579dc870e73f7c10";
+
+type Movie = {
+  poster_path: string | null;
+  adult: boolean;
+  overview: string;
+  release_date: string;
+  genre_ids: number[];
+  id: number;
+  original_title: string;
+  original_language: string;
+  title: string;
+  backdrop_path: string | null;
+  popularity: number;
+  vote_count: number;
+  video: boolean;
+  vote_average: number;
+};
+
 export default function Home() {
+  const [movies, setMovies] = useState<Movie[] | undefined>();
+
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+
+      setMovies(results);
+    })();
+  }, []);
+
   return (
     <div>
       <Seo title="Home" />
-      <h1 className="active">Hello</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
